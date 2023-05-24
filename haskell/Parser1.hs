07eb3822ea59fe8f1c60d4c7ca64553bdd807444
stephentetley <stephen.tetley@gmail.com>
2023-05-24 18:17:30 +0100

    Work on the Haskell Parser monad reference implementation.

--- haskell/Parser1.hs
+++ haskell/Parser1.hs
@@ -2,13 +2,24 @@
 
 module Parser1 where
 
+data Result err a = Ok a | Err err deriving (Eq, Ord, Show)
+
 
 type FailK (ka :: *) (st :: *) (err :: *) = st -> err -> ka
 
 type SuccessK (ka :: *) (env :: *) (st :: *) (err :: *) (a :: *) = a -> FailK ka st err -> env -> st -> ka
 
+
 newtype Parser1 (ka :: *) (env :: *) (st :: *) (err :: *) (a :: *) = 
     Parser1 (SuccessK ka env st err a -> FailK ka st err -> env -> st -> ka)
+
+runParser1 :: Parser1 (Result (st, err) (st, a))  env  st err a -> st -> env -> Result (st, err) (st, a)
+runParser1 p st env =
+    let fk = \st2 err -> Err (st2, err) in
+    let sk = \a _ _ st1 -> Ok (st1, a) in
+    let Parser1(pf) = p in
+    pf sk fk env st
+
 
 apply1 :: Parser1 ka env st err a -> SuccessK ka env st err a -> FailK ka st err -> env -> st -> ka
 apply1 p sk fk env st = 
@@ -43,3 +54,28 @@ mapError f ma =
         apply1 ma sk1 fk1 env st
     )
 
+
+ask :: Parser1 ka env st err env
+ask = Parser1(\ sk fk env st -> sk env fk env st)
+
+asks :: (env -> a) -> Parser1 ka env st err a
+asks f = Parser1(\ sk fk env st -> sk (f env) fk env st)
+
+local :: (env -> env) -> Parser1 ka env st err a -> Parser1 ka env st err a
+local f p = 
+    Parser1(\ sk fk env st ->
+        let sk1 = \x fk1 _ st1 -> sk x fk1 env st1 in
+        apply1 p sk1 fk (f env) st
+    )
+
+get :: Parser1 ka env st err st
+get = Parser1(\ sk fk env st -> sk st fk env st)
+
+gets :: (st -> a) -> Parser1 ka env st err a
+gets f = Parser1(\ sk fk env st -> sk (f st) fk env st)
+
+put :: st -> Parser1 ka env st err ()
+put st1 = Parser1(\ sk fk env st -> sk () fk env st1)
+
+puts :: (st -> st) -> Parser1 ka env st err ()
+puts f = Parser1(\ sk fk env st -> sk () fk env (f st))
