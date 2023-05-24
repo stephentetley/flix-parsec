06ccbd0a617054813ab91698b5636f12e41e2bce
stephentetley <stephen.tetley@gmail.com>
2023-05-24 20:15:57 +0100

    Haskell reference implementation - added try and lookahead.

--- haskell/Parser1.hs
+++ haskell/Parser1.hs
@@ -5,7 +5,7 @@ module Parser1 where
 data Result err a = Ok a | Err err deriving (Eq, Ord, Show)
 
 
-type FailK (ka :: *) (st :: *) (err :: *) = st -> err -> ka
+type FailK (ka :: *) (st :: *) (err :: *) = err -> st -> ka
 
 type SuccessK (ka :: *) (env :: *) (st :: *) (err :: *) (a :: *) = a -> FailK ka st err -> env -> st -> ka
 
@@ -15,7 +15,7 @@ newtype Parser1 (ka :: *) (env :: *) (st :: *) (err :: *) (a :: *) =
 
 runParser1 :: Parser1 (Result (st, err) (st, a)) env st err a -> env -> st -> Result (st, err) (st, a)
 runParser1 p env st =
-    let fk = \st2 err -> Err (st2, err) in
+    let fk = \err st2 -> Err (st2, err) in
     let sk = \a _ _ st1 -> Ok (st1, a) in
     let Parser1(pf) = p in
     pf sk fk env st
@@ -45,20 +45,20 @@ flatMap k p =
 
 
 throwError :: err -> Parser1 ka env st err a
-throwError err = Parser1(\_ fk _ st -> fk st err)
+throwError err = Parser1(\_ fk _ st -> fk err st)
 
 
 catchError :: Parser1 ka env st err a -> (err -> Parser1 ka env st err a) -> Parser1 ka env st err a
 catchError p handler = 
     Parser1(\sk fk env st ->
-        let fk1 = \fst err -> apply1 (handler err) sk fk env fst in
+        let fk1 = \err st1 -> apply1 (handler err) sk fk env st1 in
         apply1 p sk fk1 env st
     )
 
 mapError :: (err -> err) -> Parser1 ka env st err a -> Parser1 ka env st err a
 mapError f ma = 
     Parser1(\ sk fk env st ->
-        let fk1 = \pos err -> fk pos (f err) in 
+        let fk1 = \err st2 -> fk (f err) st2 in 
         let sk1 = \x _ _ st1 -> sk x fk env st1 in 
         apply1 ma sk1 fk1 env st
     )
@@ -69,10 +69,28 @@ flatMapOr :: Parser1 ka env st err a
                 -> (err -> Parser1 ka env st err b) -> Parser1 ka env st err b
 flatMapOr p pnext pelse = 
     Parser1(\ sk fk env st ->
-        let fk1 = \fst err -> apply1 (pelse err) sk fk env fst in
+        let fk1 = \err st2 -> apply1 (pelse err) sk fk env st2 in
         let sk1 = \x _ _ st1 -> apply1 (pnext x) sk fk env st1 in 
         apply1 p sk1 fk1 env st
     )
+
+-- restores state updated by first parser (Okasaki)
+lookahead :: Parser1 ka env st err a -> (a -> Parser1 ka env st err b) -> Parser1 ka env st err b
+lookahead p k = 
+    Parser1(\sk fk env st ->
+        let sk1 = \x fk1 env1 _ -> apply1 (k x) sk fk1 env1 st in
+        apply1 p sk1 fk env st
+    )
+
+-- restore state on failure (Okasaki)
+tryParse :: Parser1 ka env st err a -> Parser1 ka env st err a
+tryParse p = 
+    Parser1(\sk fk env st ->
+        let fk1 = \err _ -> fk err st in 
+        apply1 p sk fk env st
+    )
+
+
 
 ask :: Parser1 ka env st err env
 ask = Parser1(\ sk fk env st -> sk env fk env st)
