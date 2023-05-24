f7a0d7e1b51c7c0dd7c90ef871aed34c2d61e0e9
stephentetley <stephen.tetley@gmail.com>
2023-05-24 18:49:08 +0100

    Filling out the Haskell Parser monad reference implementation (flatMapOr).

--- haskell/Parser1.hs
+++ haskell/Parser1.hs
@@ -13,8 +13,8 @@ type SuccessK (ka :: *) (env :: *) (st :: *) (err :: *) (a :: *) = a -> FailK ka
 newtype Parser1 (ka :: *) (env :: *) (st :: *) (err :: *) (a :: *) = 
     Parser1 (SuccessK ka env st err a -> FailK ka st err -> env -> st -> ka)
 
-runParser1 :: Parser1 (Result (st, err) (st, a))  env  st err a -> st -> env -> Result (st, err) (st, a)
-runParser1 p st env =
+runParser1 :: Parser1 (Result (st, err) (st, a)) env st err a -> env -> st -> Result (st, err) (st, a)
+runParser1 p env st =
     let fk = \st2 err -> Err (st2, err) in
     let sk = \a _ _ st1 -> Ok (st1, a) in
     let Parser1(pf) = p in
@@ -31,7 +31,9 @@ point x =  Parser1(\sk fk env st -> sk x fk env st)
 ap :: Parser1 ka env st err (a -> b) -> Parser1 ka env st err a -> Parser1 ka env st err b
 ap pf pa = 
     Parser1(\sk fk env st ->
-        let sk1 = \f fk1 env1 st1 -> let sk2 = \a fk2 env2 st2 -> sk (f a) fk2 env2 st2 in apply1 pa sk2 fk1 env1 st1 in apply1 pf sk1 fk env st
+        let sk1 = \f fk1 env1 st1 -> let sk2 = \a fk2 env2 st2 -> sk (f a) fk2 env2 st2 in 
+                apply1 pa sk2 fk1 env1 st1 in 
+            apply1 pf sk1 fk env st
     )
 
 flatMap :: (a -> Parser1 ka env st err b) -> Parser1 ka env st err a -> Parser1 ka env st err b
@@ -46,6 +48,13 @@ throwError :: err -> Parser1 ka env st err a
 throwError err = Parser1(\_ fk _ st -> fk st err)
 
 
+catchError :: Parser1 ka env st err a -> (err -> Parser1 ka env st err a) -> Parser1 ka env st err a
+catchError p handler = 
+    Parser1(\sk fk env st ->
+        let fk1 = \fst err -> apply1 (handler err) sk fk env fst in
+        apply1 p sk fk1 env st
+    )
+
 mapError :: (err -> err) -> Parser1 ka env st err a -> Parser1 ka env st err a
 mapError f ma = 
     Parser1(\ sk fk env st ->
@@ -54,6 +63,16 @@ mapError f ma =
         apply1 ma sk1 fk1 env st
     )
 
+
+flatMapOr :: Parser1 ka env st err a 
+                -> (a -> Parser1 ka env st err b) 
+                -> (err -> Parser1 ka env st err b) -> Parser1 ka env st err b
+flatMapOr p pnext pelse = 
+    Parser1(\ sk fk env st ->
+        let fk1 = \fst err -> apply1 (pelse err) sk fk env fst in
+        let sk1 = \x _ _ st1 -> apply1 (pnext x) sk fk env st1 in 
+        apply1 p sk1 fk1 env st
+    )
 
 ask :: Parser1 ka env st err env
 ask = Parser1(\ sk fk env st -> sk env fk env st)
@@ -79,3 +98,20 @@ put st1 = Parser1(\ sk fk env st -> sk () fk env st1)
 
 puts :: (st -> st) -> Parser1 ka env st err ()
 puts f = Parser1(\ sk fk env st -> sk () fk env (f st))
+
+
+-- Testing...
+
+type ErrMsg = String
+
+runSimple :: Parser1 (Result (Int, ErrMsg) (Int, a)) String Int String a -> String -> (Result (Int, ErrMsg) (Int, a))
+runSimple p input = runParser1 p input 0
+
+test_ap01 :: Bool
+test_ap01 = runSimple (ap (point (\x -> x + 1)) (point 10)) "input string" == Ok (0, 11)
+
+
+test_local01 :: Bool
+test_local01 = runSimple (local (\_ -> "INPUT2") ask) "input string" == Ok (0, "INPUT2")
+
+
